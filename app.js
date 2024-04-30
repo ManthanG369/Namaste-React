37211428f864f7911f0c09deacf05b82ba4af874
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-04-30 14:40:12 +0530

    episode-02

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading", xyz: "abc" },
@@ -6,7 +9,7 @@ const heading = React.createElement(
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am and H1 tag"),
+    React.createElement("h1", {}, "This is namaste react🚀"),
     React.createElement("h2", {}, "I am and H2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
